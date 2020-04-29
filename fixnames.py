8f1c835aeaa9e26859de7013165b0ffa9e6e47e1
IsShai <45069507+IsShai@users.noreply.github.com>
2020-04-29 17:22:13 +0300

    update

--- fixnames.py
+++ fixnames.py
@@ -1,15 +1,37 @@
 import os
-print('Enter your folder full path:')
-workingDir = input()
-
-print('Enter thr pattern you want to remove:')
-pattern = input()
 
 
-for filename in os.listdir(workingDir):
-    if filename.endswith(".mkv"):
-        newName = filename.replace(pattern, "")
-        oldPath = os.path.join(workingDir, filename)
-        newPath = os.path.join(workingDir, newName)
-        os.rename(oldPath, newPath)
-        print(newName)
+def runOverFolder(workingDir, isApproved):
+    isFirstRun = True
+    for filename in os.listdir(workingDir):
+        if filename.endswith(".mkv"):
+            if pattern in filename:
+                newName = filename.replace(pattern, "")
+                if isApproved == False:
+                    if isFirstRun == True:
+                        print("Are you sure you want to change:")
+                        isFirstRun = False
+                    print(filename + "  -->  " + newName)
+                else:
+                    oldPath = os.path.join(workingDir, filename)
+                    newPath = os.path.join(workingDir, newName)
+                    os.rename(oldPath, newPath)
+                    print(newName)
+
+
+try:
+    print('Enter your folder full path:')
+    workingDir = input()
+
+    print('Enter thr pattern you want to remove:')
+    pattern = input()
+    # ask for user approval
+    runOverFolder(workingDir, False)
+    print('press y to continue n to cancel')
+    approval = input()
+    if approval == 'y' or approval == 'Y':
+        runOverFolder(workingDir, True)
+
+
+except IOError:
+    print('folder not found')
